ada19600be629bb88b120e9adeb4018f815430df
IshaHaider <isha.haider213@gmail.com>
2022-12-07 18:18:56 -0700

    Polish Admin interface user management

--- code/application.py
+++ code/application.py
@@ -7,13 +7,13 @@ data = mysql.connector.connect(host='127.0.0.01',
                               password='Haider1970')
 cur = data.cursor()
 
-def modify_users(value1, value2, value3, value4):
-    if value2 == value3 == value4 == None:
-        statement = f"DELETE FROM ACCOUNTS WHERE username = '{value1}';"
-    elif value4 == None:
-        statement = f"INSERT INTO ACCOUNTS VALUES ('{value1}', '{value2}', '{value3}');"
-    elif value4 != None:
-        statement = f"UPDATE ACCOUNTS SET username = '{value1}', pass = '{value2}', role = '{value3}' WHERE username = '{value4}';"
+def modify_users(user1, password, role, user2):
+    if password == role == user2 == None:
+        statement = f"DELETE FROM ACCOUNTS WHERE username = '{user1}';"
+    elif user2 == None:
+        statement = f"INSERT INTO ACCOUNTS VALUES ('{user1}', '{password}', '{role}');"
+    elif user2 != None:
+        statement = f"UPDATE ACCOUNTS SET username = '{user1}', pass = '{password}', role = '{role}' WHERE username = '{user2}';"
     cur.execute('use museum')
     cur.execute(statement)
     data.commit() 
@@ -68,87 +68,98 @@ exit = '1'
 while exit != '0':
     exit = input('Please enter 1 to continue. If you would like to exit the Main Menu, please enter 0.\n')
     if exit == '0':
+        print('Thank you for using the Museum Database.')
         break
     
-    print("Please enter your username and password.\nIf you are a guest, please enter 'none' for username and password.")
-    username = input('Username: ')
-    password = input('Password: ')
+    guest_input = input("Are you a guest user? (Y/N) ")
+    if guest_input == 'N':
+        print("Please enter your username and password.\n")
+        username = input('Username: ')
+        password = input('Password: ')
 
-    # simultaneously check if user exists and their account type
-    if (username, password, 'admin') in ACCOUNTS: # if user is admin
-        user1 = ''
-        user2 = ''
-        pw = ''
-        role = ''
-        choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Block Users\n4. Browse Database\n5. Modify Database\n0. Exit Admin Menu\n')
-        while choice != '0':
-            if choice == '1': # add users
-                insert_user = print("Please complete the SQL INSERT statement:\n INSERT INTO ACCOUNTS VALUES (new username, new password, new role)")
-                user1 = input('New Username: ')
-                pw = input('New Password: ')
-                role = input('New Role: ')
-                modify_users(user1, pw, role, None)
-                print('User successfully inserted. Returning to Admin menu...')
-                choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Block Users\n4. Modify Database\n0. Exit Admin Menu\n')
+        # simultaneously check if user exists and their account type
+        if (username, password, 'admin') in ACCOUNTS: # if user is admin
+            user_info = ['','','','']
+            choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Remove Users\n4. Browse Database\n5. Modify Database\n0. Exit Admin Menu\n')
+            while choice != '0':
+                if choice == '1': # add users
+                    insert_user = print("Please complete the SQL INSERT statement:\n INSERT INTO ACCOUNTS VALUES (new username, new password, new role)")
+                    user_info[0] = input('New Username: ')
+                    user_info[1] = input('New Password: ')
+                    user_info[1] = input('New Role: ')
+                    modify_users(user_info[0], user_info[1], user_info[1], None)
+                    print('User successfully inserted. Returning to Admin menu...')
+                    choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Remove Users\n4. Modify Database\n0. Exit Admin Menu\n')
 
-            elif choice == '2': # edit database
-                update_user = print("Please complete the SQL UPDATE statement:\n UPDATE ACCOUNTS SET username = new username, password = new password, role = new role)")
-                user1 = input('Existing Username: ')
-                user2 = input('New Username: ')
-                pw = input('New Password: ')
-                role = input('New Role: ')
-                modify_users(user2, pw, role, user1)
-                print('User successfully inserted. Returning to Admin menu...')
-                choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Block Users\n4. Modify Database\n0. Exit Admin Menu\n')
+                elif choice == '2': # edit users
+                    update_user = print("Please complete the SQL UPDATE statement:\n UPDATE ACCOUNTS SET username = new username, password = new password, role = new role)")
+                    user_info[0] = input('Existing Username: ')
+                    user_info[3] = input('New Username: ')
+                    user_info[1] = input('New Password: ')
+                    user_info[2] = input('New Role: ')
+                    modify_users(user_info[3], user_info[1], user_info[2], user_info[0])
+                    print('User successfully inserted. Returning to Admin menu...')
+                    choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Remove Users\n4. Modify Database\n0. Exit Admin Menu\n')
 
-            elif choice == '3': # delete users
-                delete_user = print("Please complete the SQL DELETE statement: DELETE FROM ACCOUNTS WHERE Username = Old Username")
-                user1 = input('Old Username: ')
-                modify_users(user1, None, None, None)
-                print('User successfully removed. Returning to Admin menu...')
-                choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Block Users\n4. Modify Database\n0. Exit Admin Menu\n')
+                elif choice == '3': # delete users
+                    delete_user = print("Please complete the SQL DELETE statement: DELETE FROM ACCOUNTS WHERE Username = Old Username")
+                    user_info[0] = input('Old Username: ')
+                    modify_users(user_info[0], None, None, None)
+                    print('User successfully removed. Returning to Admin menu...')
+                    choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Remove Users\n4. Modify Database\n0. Exit Admin Menu\n')
 
-            elif choice == '4': #search database
-                selection_string = ""
-                user_input = int(input("Please enter the corresponding number for what you would like to be displayed from Museum database: \n 1 - Art Objects \n 2 - Artists \n 3 - Borrowed Objects \n 4 - Collections Borrowed From \n 5 - Permanent Collection \n 6 - Only Paintings \n 7 - Only Sculptures \n 8 - All Other Art Objects \n 9 - Exhibition Information \n 10 - Limited Artist Information \n 11 - Oldest to Latest Art Objects \n 12 - Art Objects from Same Origin as Artist Leonardo Da Vinci \n 13 - Paintings and Sculptures with Modern Style \n 0 - Quit \n"))
-                search_database(user_input)
-                choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Block Users\n4. Modify Database\n0. Exit Admin Menu\n')
+                elif choice == '4': #search database
+                    selection_string = ""
+                    user_input = int(input("Please enter the corresponding number for what you would like to be displayed from Museum database: \n 1 - Art Objects \n 2 - Artists \n 3 - Borrowed Objects \n 4 - Collections Borrowed From \n 5 - Permanent Collection \n 6 - Only Paintings \n 7 - Only Sculptures \n 8 - All Other Art Objects \n 9 - Exhibition Information \n 10 - Limited Artist Information \n 11 - Oldest to Latest Art Objects \n 12 - Art Objects from Same Origin as Artist Leonardo Da Vinci \n 13 - Paintings and Sculptures with Modern Style \n 0 - Quit \n"))
+                    search_database(user_input)
+                    choice = input('Please select an option:\n1. Add Users \n2. Edit Users\n3. Block Users\n4. Modify Database\n0. Exit Admin Menu\n')
 
-            elif choice == '5': # modify database
-                #admin can modify tuples, can modify constraints, can modify attributes
-                table = input('Please enter the name of the table you would like to modify: ')
-                modify = input('If you would like to modify an attribute of a table, please enter 1.\nIf you would like to modify table constraints, please enter 2.\n')
+                elif choice == '5': # modify database
+                    #admin can modify tuples, can modify constraints, can modify attributes
+                    table = input('Please enter the name of the table you would like to modify: ')
+                    modify = input('Please select what you would like to do with a table:\n1. Modify Table Attributes\n2. Modify Table Constraints\n3. Modify Table Tuples\n0. Exit Database Management Menu\n')
+                    while modify != '0':
+                        if modify == '1': # modify attribute in table
+                            cur.execute(''' 
+                                        ALTER TABLE %s
+                                        ADD 
+                                        ''')
+                        
+                            #attributes = 
+                            break
+
+                        elif modify == '2': # modify constraint in table
+                            print('modify constraint in table')
+                            break
+                        
+                        elif modify == '3': # modify tuple in table
+                            print('modify tuple in table')
+                            break
+                    
+        elif (username, password, 'data entry') in ACCOUNTS: # if user is data entry type
+            choice = input('Please select an option:\n1. Browse Database\n5. Modify Database\n0. Exit Data Entry Menu\n')
+            while choice != '0':
+                if choice == '1': #search database
+                    selection_string = ""
+                    user_input = int(input("Please enter the corresponding number for what you would like to be displayed from Museum database: \n 1 - Art Objects \n 2 - Artists \n 3 - Borrowed Objects \n 4 - Collections Borrowed From \n 5 - Permanent Collection \n 6 - Only Paintings \n 7 - Only Sculptures \n 8 - All Other Art Objects \n 9 - Exhibition Information \n 10 - Limited Artist Information \n 11 - Oldest to Latest Art Objects \n 12 - Art Objects from Same Origin as Artist Leonardo Da Vinci \n 13 - Paintings and Sculptures with Modern Style \n 0 - Quit \n"))
+                    search_database(user_input)
+                    choice = input('Please select an option:\n1. Browse Database\n5. Modify Database\n0. Exit Data Entry Menu\n')
                 
-                if modify == '1': # modify attribute in table
-                    cur.execute(''' 
-                                ALTER TABLE %s
-                                ADD 
-                                ''')
-                
-                #attributes = 
-                break
-            
-    elif (username, password, 'data entry') in ACCOUNTS: # if user is data entry type
-        choice = input('Please select an option:\n1. Browse Database\n5. Modify Database\n0. Exit Data Entry Menu\n')
-        while choice != '0':
-            if choice == '1': #search database
-                selection_string = ""
-                user_input = int(input("Please enter the corresponding number for what you would like to be displayed from Museum database: \n 1 - Art Objects \n 2 - Artists \n 3 - Borrowed Objects \n 4 - Collections Borrowed From \n 5 - Permanent Collection \n 6 - Only Paintings \n 7 - Only Sculptures \n 8 - All Other Art Objects \n 9 - Exhibition Information \n 10 - Limited Artist Information \n 11 - Oldest to Latest Art Objects \n 12 - Art Objects from Same Origin as Artist Leonardo Da Vinci \n 13 - Paintings and Sculptures with Modern Style \n 0 - Quit \n"))
-                search_database(user_input)
-                choice = input('Please select an option:\n1. Browse Database\n5. Modify Database\n0. Exit Data Entry Menu\n')
-            elif choice == '2': #modify database (can only insert, delete, or update tuples)
-                break
-
-    elif (username, password, 'guest') in ACCOUNTS: # if user is end type
+                elif choice == '2': #modify database (can only insert, delete, or update tuples)
+                    break
+        
+        else: # user is not valid
+            print('Username or password are incorrect. You will be redirected to the Main Page.')
+            continue
+    
+    elif guest_input == 'Y': # if user is end type
         selection_string = "" 
         print('A guest user is limited to only viewing information from the database, they are not allowed to make any modifications.')
         user_input = int(input("Please enter the corresponding number for what you would like to be displayed from Museum database: \n 1 - Art Objects \n 2 - Artists \n 3 - Borrowed Objects \n 4 - Collections Borrowed From \n 5 - Permanent Collection \n 6 - Only Paintings \n 7 - Only Sculptures \n 8 - All Other Art Objects \n 9 - Exhibition Information \n 10 - Limited Artist Information \n 11 - Oldest to Latest Art Objects \n 12 - Art Objects from Same Origin as Artist Leonardo Da Vinci \n 13 - Paintings and Sculptures with Modern Style \n 0 - Quit \n"))
         search_database(user_input)
-
-    else: # user is not valid
-        print('Username or password are incorrect.')
-        if input('Please enter 1 to exit: ') == 1:
-            print('Thank you for using the Museum Database.')
-            exit = 1
+    
+    else:
+        print("You have entered a value that is not 'Y' or 'N'. You will be redirected to the Main Page.")
+        continue
 
 data.close()
